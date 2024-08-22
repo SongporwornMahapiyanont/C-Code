3779d699ed42f0e07cfd2bb8352cde9a3ed5758d
Songporworn Mahapiyanont <167118862+SongporwornMahapiyanont@users.noreply.github.com>
2024-08-22 14:41:47 +0700

    Update mult_table.c

--- Study/week1-week7/mult_table/mult_table.c
+++ Study/week1-week7/mult_table/mult_table.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 int main()
-{   int input,i=12;
+{   int input;
     scanf("%d",&input);
     if(input>=1 && input<=99)
     {
-        if(input*i<100)
+        if(input*12<100)
         {
             for(i=1;i<=12;i+=1)
             {
             printf("%d x %2d = %3d\n",input,i,input*i);
             }
         }
-        else if(input*i<1000)
+        else if(input*12<1000)
         {
             for(i=1;i<=12;i+=1)
             {
             printf("%d x %2d = %4d\n",input,i,input*i);
             }
         }
-        else if(input*i<10000)
+        else if(input*12<10000)
         {
             for(i=1;i<=12;i+=1)
             {
